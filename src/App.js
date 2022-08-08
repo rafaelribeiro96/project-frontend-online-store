b74879f198c34973f0b936c34e95d9751abd1b3b
L Rene <lucas.rene@hotmail.com>
2022-08-08 15:41:38 -0300

    03 comecando

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import Cart from './pages/Cart';
 import './App.css';
 
 export default class App extends Component {
   render() {
     return (
       <div className="App">
-        <p data-testid="home-initial-message">
-          Digite algum termo de pesquisa ou escolha uma categoria.
-        </p>
+        <Switch>
+          <Route exact path="/" Component={ Home } />
+          <Route exact path="/Cart" Component={ Cart } />
+        </Switch>
       </div>
     );
   }
