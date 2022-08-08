967fdd3c40d6a9cab85b76ad8ff1b30996f9a8ab
Rafael Ribeiro <rafaelfelipe.r@hotmail.com>
2022-08-08 16:48:45 -0300

    Requirement 04 start

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Categories from './Categories';
 import './App.css';
 
 export default class App extends Component {
@@ -8,6 +9,7 @@ export default class App extends Component {
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
+        <Categories />
       </div>
     );
   }
