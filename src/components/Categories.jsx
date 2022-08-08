2a687aa8ee809a9712ecf6a4ed637eca89a41d02
Tárcio Rezende <tarciosrezendd@gmail.com>
2022-08-08 19:40:30 -0300

    requisito 06

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,38 +1,59 @@
 import React, { Component } from 'react';
-import { getCategories } from '../services/api';
+import { getCategories, showCategories } from '../services/api';
 
 export default class Categories extends Component {
-state = {
-  categoriesArray: [],
-}
+  state = {
+    categoriesArray: [],
+    productByCategory: [],
+  }
 
-componentDidMount() {
-  this.setState(async () => {
-    const saveCategories = await getCategories();
-    return this.setState({ categoriesArray: saveCategories });
-  });
-}
+  componentDidMount() {
+    this.setState(async () => {
+      const saveCategories = await getCategories();
+      return this.setState({ categoriesArray: saveCategories });
+    });
+  }
 
-render() {
-  const { categoriesArray } = this.state;
-  return (
+  handleClick = async ({ target }) => {
+    const { id } = target;
+    console.log(id);
+    const categorieId = await showCategories(id);
+    const { results } = await categorieId;
+    return this.setState({ productByCategory: results });
+  }
 
-    <form>
-      <h3>Categorias</h3>
-      <ul>
-        {categoriesArray.map((category) => (
-          <li key={ category.id }>
-            <button
-              type="submit"
-              data-testid="category"
-              id={ category.id }
-            >
-              { category.name }
-            </button>
+  render() {
+    const { categoriesArray, productByCategory } = this.state;
+    return (
+
+      <form>
+        <h3>Categorias</h3>
+        <ul>
+          {categoriesArray.map((category) => (
+            <li key={ category.id }>
+              <button
+                type="button"
+                data-testid="category"
+                id={ category.id }
+                onClick={ this.handleClick }
+              >
+                {category.name}
+              </button>
+            </li>
+          ))}
+        </ul>
+        {console.log(productByCategory)}
+        {productByCategory.map(({ id, title, thumbnail, price }) => (
+          <li
+            key={ id }
+            data-testid="product"
+          >
+            <p>{ title }</p>
+            <img alt="imagem" src={ thumbnail } />
+            <p>{ price }</p>
           </li>
         ))}
-      </ul>
-    </form>
-  );
-}
+      </form>
+    );
+  }
 }
