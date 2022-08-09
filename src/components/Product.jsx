6a7366080e034088731355d75d3ada5632e46bb7
L Rene <lucas.rene@hotmail.com>
2022-08-09 20:24:12 -0300

    08-parcial, button nao renderiza

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -3,6 +3,23 @@ import propTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 class Product extends Component {
+  addToCart = async (id, title, thumbnail, price) => {
+    const cartItem = { id, title, thumbnail, price, quantity: 1 };
+    const actualCart = JSON.parse(localStorage.getItem('cart'));
+    let newCart = [];
+    if (actualCart) {
+      newCart = [cartItem, ...actualCart];
+      console.log(newCart);
+      if (actualCart.some((item) => item.id === cartItem.id)) {
+        newCart = actualCart;
+        newCart.forEach((item, i) => {
+          if (item.id === cartItem.id) newCart[i].quantity += 1;
+        });
+      }
+      localStorage.setItem('cart', JSON.stringify(newCart));
+    } else localStorage.setItem('cart', JSON.stringify([cartItem]));
+  };
+
   render() {
     const { name, img, price, id } = this.props;
     return (
@@ -10,6 +27,13 @@ class Product extends Component {
         <p>{ name }</p>
         <img alt="imagem" src={ img } />
         <p>{ price }</p>
+        <button
+          type="button"
+          data-testid="product-add-to-cart"
+          onClick={ () => this.addToCart(id, title, thumbnail, price) }
+        >
+          Adicionar ao Carrinho
+        </button>
         <Link
           data-testid="product-detail-link"
           to={ `/ProductDetails/${id}` }
